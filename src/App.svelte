02fbd490a593a3f6391c7f092d995370ea95db63
Senaka Amarakeerthi <senakahks@gmail.clom>
2024-08-14 08:03:07 +1200

    Made header and footer full width

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   import { onMount } from "svelte";
   import Navbar from "./components/Navbar.svelte";
   import Footer from "./components/Footer.svelte";
-
+  import Body from "./components/Body.svelte";
 
   let data;
   if (import.meta.env.VITE_API_ENDPOINT) {
@@ -16,28 +16,11 @@
   }
 </script>
 
-<div class="container">
-
-  <div class="p-8 overflow-hidden bg-gray-50 dark:bg-gray-900">
-    <Navbar />
-    <Router {routes} />
-
-    {#if data}
-      <p>Data:</p>
-      <ul>
-        {#each Object.entries(data) as [key, value]}
-          <li>{key}: {value}</li>
-        {/each}
-      </ul>
-    {/if}
-    <Footer />
-  </div>
+<div class="overflow-hidden bg-gray-50 dark:bg-gray-900">
+  <Navbar />
+  <Router {routes} />
+    <Body />
+  <Footer />
 </div>
 
-<style>
-  .container {
-    max-width: 1200px;
-    margin: 0 auto;
-    border-radius: 8px; /* Optional rounded corners */
-  }
-</style>
+
